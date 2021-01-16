650fd9fc897db58668e6f2c78bfffb424640211b
sunakan <sunakan03@gmail.com>
2021-01-16 06:55:39 +0000

    npm run lintでeslint

---
+++ hello-cdk/.prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  trailingComma: "es5", // 末尾のカンマの扱い
+  tabWidth: 2,          // インデントのスペースの数
+  semi: false,
+  singleQuote: true,    // 文字列の括り文字をシングルクォートで統一するか
+};
--- hello-cdk/bin/hello-cdk.ts
+++ hello-cdk/bin/hello-cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { HelloCdkStack } from '../lib/hello-cdk-stack';
+import 'source-map-support/register'
+import * as cdk from '@aws-cdk/core'
+import { HelloCdkStack } from '../lib/hello-cdk-stack'
 
-const app = new cdk.App();
-new HelloCdkStack(app, 'HelloCdkStack');
+const app = new cdk.App()
+new HelloCdkStack(app, 'HelloCdkStack')
